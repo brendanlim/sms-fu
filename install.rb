d16925d6f60669d492005b419df212d26b3d21bd
Brendan G. Lim <brendan@intridea.com>
2008-04-19 01:17:51 -0500

    Adding in kajeet to carrier list and updated README

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
 require 'ftools'
 require 'fileutils'
-puts IO.read(File.join(File.dirname(__FILE__), 'README'))
+puts IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 File.copy(File.dirname(__FILE__)+'/templates/sms_fu.yml', File.dirname(__FILE__)+'/../../../config')
