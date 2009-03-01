15d31df11aa0d77a688d44955daf9647aad70b2b
Brent Collier <brentmc79@gmail.com>
2009-02-28 22:47:53 -0500

    fixin syntax error in helper

--- lib/sms_fu_helper.rb
+++ lib/sms_fu_helper.rb
@@ -2,7 +2,7 @@ module SMSFuHelper
   # Returns a collection of carriers to be used in your own select tag
   # e.g., <%= f.select :mobile_carrier, carrier_collection %>
   def carrier_collection
-    SMSFu.carriers.sort.collect{ |carrier| [carrier[1]["name"], carrier[0] }
+    SMSFu.carriers.sort.collect{ |carrier| [carrier[1]["name"], carrier[0]] }
   end
   
   # Returns a formatted select box filled with carriers
