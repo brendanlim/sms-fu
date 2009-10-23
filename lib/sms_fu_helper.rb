f93349785ebcc991bec1b463712d7cfef04fb91b
pboling <peter.boling@peterboling.com>
2009-10-23 03:00:06 -0400

    default phrase should not force a blank option tag, now it is optional

--- lib/sms_fu_helper.rb
+++ lib/sms_fu_helper.rb
@@ -10,8 +10,10 @@ module SMSFuHelper
   # - name => name of the method in which you want to store the carrier name
   # - phrase => default selected blank option in select box 
   # - selected => carrier to pre-select
-  def carrier_select(name = :mobile_carrier, phrase = "Select a Carrier", selected = nil)
-    select_tag name, options_for_select([phrase,nil]+carrier_collection, selected || phrase)
+  # - include_blank => Do you want the blank option tag (in ADDITION to the value-less 'phrase' option above)?
+  def carrier_select(name = :mobile_carrier, phrase = "Select a Carrier", selected = nil, include_blank = true)
+    options = phrase.nil? ? carrier_collection : include_blank ? [phrase,nil] + carrier_collection : [phrase] + carrier_collection
+    select_tag name, options_for_select(options, selected || phrase)
   end
 
 end
